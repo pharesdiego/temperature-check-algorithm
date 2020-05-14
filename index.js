24162302671f04c335d6bbab698944c3fdcd2fb8
Diego <diego@Diegos-MacBook-Pro.local>
2020-05-14 16:26:13 -0500

    Add input for detected forehead temperature

--- index.js
+++ index.js
@@ -4,57 +4,49 @@
  */
 
 const fetch = require("node-fetch");
-// const readline = require("readline")
-//   .createInterface({
-//     input: process.stdin,
-//     output: process.stdout,
-//   });
-
+const readline = require("readline")
+  .createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
 const url = "http://api.openweathermap.org/data/2.5/weather?q=medellin,co&APPID=91f0c1168a43d953d36187844439b308&units=metric";
+
 const FHIncreaseRateForEachOneDegreeIncreaseInAT = 0.5;
+
 const hardwareAccuracyRange = 1.5;
+
 const maxATAtWhichFTStopsIncreasing = 40;
+
 const foreheadTemperatureAtFreezingTemperature = 24.9;
-const isInRange = (baseNumber, range) => (testedNumber) => {
-  return testedNumber < (baseNumber + range);
-};
 
 const getPercentage = (x, y, z) => (x * y) / z;
+
+const isInRange = (min, tested, max) => min <= tested && tested <= max;
 
 const getResult = (sum, at) =>
   ((FHIncreaseRateForEachOneDegreeIncreaseInAT - sum) * at) + foreheadTemperatureAtFreezingTemperature;
 
-// ((0.3 + (0.2 - 0.2)) * 40) + 24.9
-// ((0.5 - 0.2 or R2) * 40) + 24.9
-// ((0.5) * 2) + 24.9 => for 2 degree
 (async () => {
   const data = await (await fetch(url)).json();
-  // const at = Math.round(data.main.feels_like < 0 ? 0 : data.main.feels_like);
-  const at = 20.5;
+  const at = Math.round(data.main.feels_like < 0 ? 0 : data.main.feels_like);
+  const expectedFTBasedOnAT =
+    Math.round(getResult(getPercentage(getPercentage(at, 100, maxATAtWhichFTStopsIncreasing), 0.199, 100), at) * 10) / 10;
 
-  // const temperatures = Array.from({ length: 21 }, (x, i) => i + i);
-  // console.log(
-  //  JSON.stringify( temperatures.map(t => ({ t, result: Math.round(getResult(getPercentage(getPercentage(t, 100, maxATAtWhichFTStopsIncreasing), 0.199, 100), t) * 10) / 10 })), null, 2)
-  // )
+  const foreheadTemperature = await new Promise((resolve) => {
+    readline.question("Sensor's detected temperature:", (detectedTemperature) => {
+      resolve(detectedTemperature);
+      readline.close();
+    });
+  });
 
-  const result = Math.round(getResult(getPercentage(getPercentage(at, 100, maxATAtWhichFTStopsIncreasing), 0.199, 100), at) * 10) / 10;
-// 33.8 - 2.5
+  const [minExpectedFT, maxExpectedFT] = [expectedFTBasedOnAT - hardwareAccuracyRange * 2, expectedFTBasedOnAT + hardwareAccuracyRange];
+
   console.log(
-    "your ft should be between: ",
-    `${(result - hardwareAccuracyRange).toFixed(1)} - ${(result + hardwareAccuracyRange).toFixed(1)}`,
-    "\n because the AT is: ",
-    at,
+    `Forehead temperature should be between ${minExpectedFT} and ${maxExpectedFT}\n`,
+    isInRange(minExpectedFT, +foreheadTemperature, maxExpectedFT)
+      ? `Your forehead temperature (${foreheadTemperature}°) is between normal range`
+      : `Your forehead temperature (${foreheadTemperature}°) is not in the normal range`
   );
-
-
-  // const isInNormalForeheadTemperature = isInRange(maxNormalForeheadTemperature, 2.5);
-  // const foreheadTemperature = await new Promise((resolve) => {
-  //   readline.question("Sensor's detected temperature:", (detectedTemperature) => {
-  //     resolve(detectedTemperature);
-  //     readline.close();
-  //   });
-  // });
-
 })();
 
